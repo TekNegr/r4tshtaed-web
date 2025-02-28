d9fa9d5dfb5f3719a5cdcde3d5f43275c0093bab
TekNegr <h.social74@gmail.com>
2025-03-01 00:07:09 +0100

    Ajout Filament

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TEST</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="">
-        
+            Page de test
     </div>
     
 </body>
